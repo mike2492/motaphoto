d06a7b021b1cb28c97116d6889cb2e062ed34ed0
Michael Laau <michael.laau2401@gmail.com>
2025-01-03 19:26:11 +0100

    Intégration de la zone de contenu

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 jQuery('.btn-contact').on('click', function(){
     jQuery('#myModal').css('display', 'block');
     jQuery('body').css('overflow', 'hidden');
+    jQuery('.ref').val(jQuery('.reference').text());
+});
+
+jQuery('.btn-contact-2').on('click', function(){
+    jQuery('#myModal').css('display', 'block');
+    jQuery('body').css('overflow', 'hidden');
+    jQuery('.ref').val(jQuery('.reference').text());
 });
 
 jQuery('.btn-close').on('click', function(){
@@ -13,4 +20,22 @@ jQuery(window).on('click', function(e){
         jQuery('#myModal').css('display', 'none');
         jQuery('body').css('overflow', 'auto');
     }
+});
+
+jQuery('.prev').on('mouseover', function(){
+    jQuery('.currentImg').attr('src', jQuery('.prevImg').attr('src'));
+    jQuery('.currentImg').css('display', 'flex');    
+});
+
+jQuery('.prev').on('mouseout', function(){
+    jQuery('.currentImg').css('display', 'none');
+});
+
+jQuery('.next').on('mouseover', function(){
+    jQuery('.currentImg').attr('src', jQuery('.nextImg').attr('src'));
+    jQuery('.currentImg').css('display', 'flex');
+});
+
+jQuery('.next').on('mouseout', function(){  
+    jQuery('.currentImg').css('display', 'none');
 });
--- single-photo.php
+++ single-photo.php
@@ -3,5 +3,102 @@ get_header();
 ?>
 
 <?php
+$post = get_post();
+$prev_post = get_previous_post();
+$next_post = get_next_post();
+$categorie = get_the_terms($post->ID, 'categorie');
+$format = get_the_terms($post->ID, 'format');
+$fields = get_field_objects($post->ID);
+$args = array(
+    'post_type' => 'photo',
+    'p' => $post->ID,
+);
+$single = new WP_Query($args);
+$args2 = array(
+    'post_type' => 'photo',
+    'posts_per_page' => 1,
+    'order' => 'DESC',
+);
+$q2 = new WP_Query($args2);
+$args3 = array(
+    'post_type' => 'photo',
+    'posts_per_page' => 1,
+    'order' => 'ASC',
+);
+$q3 = new WP_Query($args3);
+?>
+
+<?php if($single->have_posts()): ?>
+    <div class="single">
+        <?php while($single->have_posts()) : $single->the_post(); ?>
+            <div class="photo-infos">
+                <h2><?= the_title(); ?></h2>
+                <p><?= $fields['reference']['label']; ?> : <span class="reference"><?= $fields['reference']['value']; ?></span></p>
+                <p><?= $categorie[0]->taxonomy; ?> : <?= $categorie[0]->name; ?></p>
+                <p><?= $format[0]->taxonomy; ?> : <?= $format[0]->name; ?></p>
+                <p><?= $fields['type']['label']; ?> : <?= $fields['type']['value']; ?></p>
+                <p>Année : <?= get_the_date('Y'); ?></p>
+            </div>
+            <div class="photo">
+                <?= the_post_thumbnail(); ?>
+            </div>
+        <?php endwhile; ?>
+    </div>
+<?php endif; ?>
+
+<div class="bottom-bar">
+    <div class="bottom-contact">
+        <p>Cette photo vous intéresse ?</p>
+        <button class="btn-contact-2">Contact</button>
+    </div>
+    <div class="bottom-navigation">
+        <div class="images">
+            <?php if(empty($prev_post)): ?>
+                <?php if($q2->have_posts()): ?>
+                    <?php while($q2->have_posts()) : $q2->the_post(); ?>
+                        <img src="<?= get_the_post_thumbnail_url();  ?>" alt="" class="prevImg">
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            <?php else: ?>
+                <img src="<?= get_the_post_thumbnail_url($prev_post->ID); ?>" alt="" class="prevImg">
+            <?php endif; ?>
+
+            <?php if(empty($next_post)): ?>
+                <?php if($q3->have_posts()): ?>
+                    <?php while($q3->have_posts()) : $q3->the_post(); ?>
+                        <img src="<?= get_the_post_thumbnail_url(); ?>" alt="" class="nextImg">
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            <?php else: ?>
+                <img src="<?= get_the_post_thumbnail_url($next_post->ID); ?>" alt="" class="nextImg">
+            <?php endif; ?>
+
+            <img src="" alt="" class="currentImg">
+        </div>
+        <div class="arrows">
+            <?php if(empty($prev_post)): ?>
+                <?php if($q2->have_posts()): ?>
+                    <?php while($q2->have_posts()) : $q2->the_post(); ?>
+                        <a class="prev" href="<?= get_permalink(); ?>"><img src="<?= get_stylesheet_directory_uri() . '/img/back.png' ?>" alt=""></a>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            <?php else: ?>
+                <a class="prev" href="<?= get_permalink($prev_post->ID); ?>"><img src="<?= get_stylesheet_directory_uri() . '/img/back.png' ?>" alt=""></a>
+            <?php endif; ?>
+
+            <?php if(empty($next_post)): ?>
+                <?php if($q3->have_posts()): ?>
+                    <?php while($q3->have_posts()) : $q3->the_post(); ?>
+                        <a class="next" href="<?= get_permalink(); ?>"><img src="<?= get_stylesheet_directory_uri() . '/img/next.png' ?>" alt=""></a>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            <?php else: ?>
+                <a class="next" href="<?= get_permalink($next_post->ID); ?>"><img src="<?= get_stylesheet_directory_uri() . '/img/next.png' ?>" alt=""></a>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
+
+<?php
 get_footer();
 ?>
