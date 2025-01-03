6ac0a7cb6856ddbb44f7fff5af1a70c6312d6587
Michael Laau <michael.laau2401@gmail.com>
2025-01-03 19:49:15 +0100

    Ajout du hero (entête de page)

--- index.php
+++ index.php
@@ -3,5 +3,22 @@ get_header();
 ?>
 
 <?php
+$args = array(
+    'post_type' => 'photo',
+    'posts_per_page' => 1,
+    'orderby' => 'rand',
+);
+$hero = new WP_Query($args);
+?>
+
+<?php if($hero->have_posts()): ?>
+    <div class="hero">
+        <?php while($hero->have_posts()) : $hero->the_post(); ?>
+            <?= the_post_thumbnail(); ?>
+        <?php endwhile; ?>
+    </div>
+<?php endif; ?>
+
+<?php
 get_footer();
 ?>
