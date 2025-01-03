d3759aaaeeec9934c539159d6b99704792691116
Michael Laau <michael.laau2401@gmail.com>
2025-01-03 16:22:04 +0100

    Ajout des polices et de la modale de contact

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 function load_scripts(){
     wp_enqueue_style('mota', get_stylesheet_directory_uri() . '/style.css');
+    wp_enqueue_script('scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array('jquery'), '3.7.1', true);
 }
 add_action('wp_enqueue_scripts', 'load_scripts');
 ?>
