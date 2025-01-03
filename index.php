e1864ec788afd5410eaa4b173d3fc012037f9ad3
Michael Laau <michael.laau2401@gmail.com>
2025-01-03 19:59:27 +0100

    Ajout de la liste des photos

--- index.php
+++ index.php
@@ -9,12 +9,25 @@ $args = array(
     'orderby' => 'rand',
 );
 $hero = new WP_Query($args);
+$args2 = array(
+    'post_type' => 'photo',
+    'posts_per_page' => 8,
+);
+$photos = new WP_Query($args2);
 ?>
 
 <?php if($hero->have_posts()): ?>
     <div class="hero">
         <?php while($hero->have_posts()) : $hero->the_post(); ?>
             <?= the_post_thumbnail(); ?>
+        <?php endwhile; ?>
+    </div>
+<?php endif; ?>
+
+<?php if($photos->have_posts()): ?>
+    <div class="liste-hero">
+        <?php while($photos->have_posts()) : $photos->the_post(); ?>
+            <?= get_template_part('templates_part/photo_block'); ?>
         <?php endwhile; ?>
     </div>
 <?php endif; ?>
