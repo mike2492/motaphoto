6b7daa8b07bd9b9513500a983806ac6f3e4c4448
mike2492 <michael.laau2401@gmail.com>
2025-03-03 17:04:39 +0100

    Ajout des polices et de la modale de contact

---
+++ templates_part/modal.php
@@ -0,0 +1,11 @@
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="btn-close">&times;</span>
+    <img src="<?= get_stylesheet_directory_uri() . '/img/contact-header.png' ?>" alt="Image contact header" class="contact-header">
+    <?= do_shortcode('[contact-form-7 id="a926090" title="Formulaire de contact"]'); ?>
+  </div>
+
+</div>
