22453d916487c610b73753a335ab992c418d4377
mike2492 <michael.laau2401@gmail.com>
2025-03-05 17:18:57 +0100

    Ajout du hero (entête de page)

--- index.php
+++ index.php
@@ -3,5 +3,21 @@ get_header();
 ?>
 
 <?php
+$hero = new WP_Query([
+    'post_type' => 'photo',
+    'posts_per_page' => 1,
+    'orderby' => 'rand',
+]);
+?>
+
+<?php if($hero->have_posts()): ?>
+    <div class="hero">
+        <?php while($hero->have_posts()) : $hero->the_post(); ?>
+            <?= the_post_thumbnail(); ?>
+        <?php endwhile; ?>
+    </div>
+<?php endif; ?>
+
+<?php
 get_footer();
 ?>
