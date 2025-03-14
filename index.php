1cb4facdaeca7aa69c6e886045a9cceecc029195
mike2492 <michael.laau2401@gmail.com>
2025-03-14 11:58:53 +0100

    Ajout de la liste des photos

--- index.php
+++ index.php
@@ -8,12 +8,25 @@ $hero = new WP_Query([
     'posts_per_page' => 1,
     'orderby' => 'rand',
 ]);
+
+$photos = new WP_Query([
+    'post_type' => 'photo',
+    'posts_per_page' => -1,
+]);
 ?>
 
 <?php if($hero->have_posts()): ?>
     <div class="hero">
         <?php while($hero->have_posts()) : $hero->the_post(); ?>
             <?= the_post_thumbnail(); ?>
+        <?php endwhile; ?>
+    </div>
+<?php endif; ?>
+
+<?php if($photos->have_posts()): ?>
+    <div class="liste-photos">
+        <?php while($photos->have_posts()) : $photos->the_post(); ?>
+            <?= get_template_part('templates_part/photo_block'); ?>
         <?php endwhile; ?>
     </div>
 <?php endif; ?>
